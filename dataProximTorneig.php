f698cf173ae55478135cb72482eedc85e8ceb13a
lbosch <lbosch@icra.cat>
2015-07-30 02:43:17 +0200

    data proxim torneig canviada

--- dataProximTorneig.php
+++ dataProximTorneig.php
@@ -1,9 +1,9 @@
 <?php
 
 //data: va dins un new Date(string) de javascript
-$dataProximTorneig="July 5, 2015 10:00:00";
+$dataProximTorneig="August 2, 2015 10:00:00";
 
 //nom proxim torneig
-$nomProximTorneig="Torneig 6";
+$nomProximTorneig="Torneig 7";
 
 ?>
