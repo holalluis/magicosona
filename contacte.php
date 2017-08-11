c66e8389e7f7d4db8ab056d686216ad52eb9b7bc
lbosch <lbosch@icra.cat>
2017-08-11 19:55:17 +0200

    canvi data

--- contacte.php
+++ contacte.php
@@ -14,28 +14,42 @@
 
 <?php
 	//ENTRADA
-	if(isset($_POST['nom']))
-	{
+	/*
+	if(isset($_POST['nom'])) {
 		$nom	    = mysql_real_escape_string($_POST['nom']);
 		$mail	    = mysql_real_escape_string($_POST['mail']);
 		$missatge = mysql_real_escape_string($_POST['missatge']);
 		mail('holalluis@gmail.com','Contacte formulari magicosona',"$nom\n$mail\n\n$missatge") or die('Error. Mail no enviat. Torna-ho a intentar més tard');
 		die('Gràcies per contactar amb nosaltres. Ens posarem en contacte amb tu en el mínim temps possible.');
 	}
+	*/
 ?>
 
 <!--nota-->
-<div style=padding:0.5em>
-	Pots contactar directament per
-	<div>
+<div style=padding:0.5em id=nota>
+	<style>
+		#nota .item {
+			margin:10px 0;
+		}
+	</style>
+	Contacta directament al whatsapp amb el següent enllaç:
+	<div class=item>
 		<a target=_blank style=font-size:23px href="https://api.whatsapp.com/send?phone=34677626363">
-			Whatsapp
+			Whatsapp (677626363)
 		</a>
 	</div>
+	o per mail: 
+	<div class=item>
+		<a target=_blank style=font-size:23px href="mailto:holalluis@gmail.com">
+			holalluis@gmail.com
+		</a>
+	</div>
+	<!--
 	o amb el següent formulari:
+	-->
 </div>
 
-<!--formulari-->
+<!--formulari
 <form method=POST>
 	<table style=margin:auto>
 		<style>
@@ -63,3 +77,4 @@
 	//focus a nom
 	document.getElementsByName('nom')[0].focus();
 </script>
+-->
