69dfb763b7d9a2782e2c9627df1422acc2632a8d
lbosch <lbosch@icra.cat>
2017-10-26 01:31:16 +0200

    update date proxim torneit

--- dataProximTorneig.php
+++ dataProximTorneig.php
@@ -1,5 +1,5 @@
 <?php
-	$proxim="2017-10-01";
+	$proxim="2017-11-12";
 	$proximUnix=strtotime($proxim);
 	$dataProximTorneig=date("d/m/Y",$proximUnix)."";
 	$falten=ceil(($proximUnix-time())/86400);
