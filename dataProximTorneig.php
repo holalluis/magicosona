71e5b5657016f57ce06ec9135c562cc20e971cfb
holalluis <holalluis@gmail.com>
2018-09-04 23:22:20 +0200

    data proxim torneig

--- dataProximTorneig.php
+++ dataProximTorneig.php
@@ -1,6 +1,6 @@
 <?php
-	$proxim="2018-03-25";
-	$proximUnix=strtotime($proxim);
-	$dataProximTorneig=date("d/m/Y",$proximUnix)."";
-	$falten=ceil(($proximUnix-time())/86400);
+  $proxim="2018-09-23";
+  $proximUnix=strtotime($proxim);
+  $dataProximTorneig=date("d/m/Y",$proximUnix);
+  $falten=ceil(($proximUnix-time())/86400);
 ?>
