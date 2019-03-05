ea9a79a4c9afc8e014dee301db7a5a659f8db10a
holalluis <holalluis@gmail.com>
2019-03-05 16:38:00 +0100

    update data

--- dataProximTorneig.php
+++ dataProximTorneig.php
@@ -1,5 +1,5 @@
 <?php
-  $proxim="2019-03-03";
+  $proxim="2019-03-31";
   $proximUnix=strtotime($proxim);
   $dataProximTorneig=date("d/m/Y",$proximUnix);
   $falten=ceil(($proximUnix-time())/86400);
